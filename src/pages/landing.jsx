18244fa15f59edf2d419475238d00465f41fd516
ArjunDevSingla <73811814+ArjunDevSingla@users.noreply.github.com>
2022-03-17 01:30:48 +0530

    Added Carousel

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Navbar from "../components/navbar";
+// import LandCar from "../components/home/land-carousel";
+// import QuickLinks from "../components/home/quicklinks";
+import Bureaus from "../components/home/bureaus";
+// import Initiatives from "../components/home/initiatives";
+// import Eminent from "../components/home/eminent";
+// import Footer from "../components/footer";
 
 export default function landing() {
     return (
         <div>
-            landing
-            <Navbar />
+            {/* <Navbar /> */}
+            {/* <LandCar />
+            <QuickLinks /> */}
+            <Bureaus />
+            {/* <Initiatives />
+            <Eminent />
+            <Footer /> */}
         </div>
     );
 }
